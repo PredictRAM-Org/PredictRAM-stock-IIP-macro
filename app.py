abf21d663690e6dbcd988fd7f8ffa5e771cc92e5
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-24 01:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,24 @@ import matplotlib.pyplot as plt
 # Load stock data
 stock_data = pd.read_json("stock.json")
 
+# Print the unique values in the 'Date' column for debugging
+print("Unique values in 'Date' column before conversion:", stock_data['Date'].unique())
+
+# Error handling for converting 'Date' column to datetime format
+try:
+    stock_data['Date'] = pd.to_datetime(stock_data['Date'], errors='raise')
+except ValueError as e:
+    st.error(f"Error: Unable to convert 'Date' column to datetime format in stock_data. Error message: {e}")
+    st.stop()
+
+# Print the unique values in 'Date' column after conversion for debugging
+print("Unique values in 'Date' column after conversion:", stock_data['Date'].unique())
+
 # Load macro data
 macro_data = pd.read_excel("macro_data.xlsx")
 
 # Load IIP data
 iip_data = pd.read_csv("IIP.csv")
-
-# Error handling for converting 'Date' column to datetime format
-try:
-    stock_data['Date'] = pd.to_datetime(stock_data['Date'], errors='raise')
-except ValueError:
-    st.error("Error: Unable to convert 'Date' column to datetime format in stock_data.")
-    st.stop()
 
 # Ensure 'Date' column is in datetime format for proper merging
 macro_data['Reporting Date'] = pd.to_datetime(macro_data['Reporting Date'])
@@ -29,7 +35,6 @@ iip_data['Date'] = pd.to_datetime(iip_data['Date'])
 # Combine data based on common date columns
 combined_data = pd.merge(stock_data, macro_data, how="inner", left_on="Date", right_on="Reporting Date")
 combined_data = pd.merge(combined_data, iip_data, how="inner", on="Date")
-
 
 # User interface
 st.title("Stock Analysis App")
